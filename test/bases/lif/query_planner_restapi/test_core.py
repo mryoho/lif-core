d7903f2284520143d8921edc26f6bffd2bb7b602
Gary Roybal <gary.roybal@gmail.com>
2025-12-02 15:38:03 -0700

    Chore: Fix unit test that was recently broken (#19)

--- test/bases/lif/query_planner_restapi/test_core.py
+++ test/bases/lif/query_planner_restapi/test_core.py
@@ -1,5 +1,17 @@
-from lif.query_planner_restapi import core
+import os
+import unittest
+from unittest.mock import patch
 
 
-def test_sample():
-    assert core is not None
+class TestMyModule(unittest.TestCase):
+    @patch.dict(
+        os.environ,
+        {
+            "LIF_QUERY_PLANNER_INFORMATION_SOURCES_CONFIG_PATH": os.path.dirname(__file__)
+            + "/test_information_sources_config.yml"
+        },
+    )
+    def test_core(self):
+        from lif.query_planner_restapi import core
+
+        assert core is not None
